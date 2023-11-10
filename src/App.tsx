0af5374bef50e7341dfef993deb378a9eaeee933
Camden Withrow <camdenwithrow@Camdens-MacBook-Pro.local>
2023-11-09 18:08:35 -0600

    responsive and design updates

--- src/App.tsx
+++ src/App.tsx
@@ -27,71 +27,61 @@ function App() {
   }, [])
 
   return (
-      <div className="min-h-screen w-screen relative text-slate-200 bg-slate-500/40">
-        <div className="absolute inset-0 -z-50 bg-center bg-cover bg-hero"></div>
-        <div className="absolute top-0 left-0 right-0 h-96 -z-40  bg-gradient-to-t from-transparent to-slate-900"></div>
-        <div className="w-full py-4 px-8 flex items-center justify-between ">
-          <h2 className=" text-xl font-bold">App Drawer</h2>
-          <button className="py-2 px-3 rounded-lg border border-slate-200 transition hover:bg-slate-200 hover:text-slate-900 active:bg-slate-600 active:text-slate-200 active:border-slate-600">Log out</button>
+    <div className="min-h-screen w-screen relative text-slate-200 bg-slate-500/40">
+      <div className="absolute inset-0 -z-50 bg-center bg-cover bg-hero"></div>
+      <div className="absolute top-0 left-0 right-0 h-96 -z-40  bg-gradient-to-t from-transparent to-slate-900"></div>
+      <div className="w-full py-4 px-8 flex items-center justify-between ">
+        <h2 className=" text-xl font-bold">App Drawer</h2>
+        <button className="py-2 px-3 rounded-lg border border-slate-200 transition hover:bg-slate-200 hover:text-slate-900 active:bg-slate-600 active:text-slate-200 active:border-slate-600">Log out</button>
+      </div>
+      <div className="w-full p-8 sm:p-0 sm:w-auto mt-24 flex justify-center">
+        <div className="w-full sm:w-auto grid grid-cols-2 sm:grid-cols-3 gap-8 md:grid-cols-4">
+          {apps.map((app) => (
+            <a
+              key={app.id}
+              // href={app.url}
+              href="#"
+              className="relative p-4 flex flex-col justify-center items-center bg-slate-500/20 cursor-pointer rounded-lg group"
+            >
+              <span className="absolute inset-0 shadow shadow-slate-500 rounded-lg group-active:opacity-0"></span>
+              <span className="absolute inset-0 rounded-lg shadow-lg shadow-slate-500 opacity-0 duration-100 transition-opacity group-hover:opacity-60 group-active:opacity-0 active:transition-none"></span>
+              <img src={app.logo} alt={`${app.name} logo`} className="h-24 w-24 sm:h-32 sm:w-32" />
+              <h4 className="mt-2 font-extrabold text-xs uppercase">{app.name}</h4>
+              <button onClick={() => setSelectedApp(app)} className='absolute top-2 right-1 h-8 w-8 z-1'>
+                <MoreInfo className="stroke-slate-200 w-8 h-8 border-none" />
+              </button>
+            </a>
+          ))}
         </div>
-        <div className="w-full p-8 sm:p-0 sm:w-auto mt-24 flex justify-center">
-          <div className="w-full sm:w-auto grid grid-cols-3 gap-8 md:grid-cols-4">
-            {apps.map((app) => (
-              <a
-                key={app.id}
-                // href={app.url}
-                href="#"
-                className="relative p-4 flex flex-col justify-center items-center bg-slate-500/20 cursor-pointer rounded-lg group"
-              >
-                <span className="absolute inset-0 shadow shadow-slate-500 rounded-lg group-active:opacity-0"></span>
-                <span className="absolute inset-0 rounded-lg shadow-lg shadow-slate-500 opacity-0 duration-100 transition-opacity group-hover:opacity-60 group-active:opacity-0 active:transition-none"></span>
-                <img src={app.logo} alt={`${app.name} logo`} className="h-32 w-32" />
-                <h4 className="mt-2 font-extrabold text-xs uppercase">{app.name}</h4>
-                <button onClick={() => setSelectedApp(app)} className='absolute top-2 right-1 h-8 w-8 z-1'>
-                  <MoreInfo className="stroke-slate-200 w-8 h-8 border-none" />
-                </button>
-              </a>
-            ))}
-          </div>
-        </div>
-        {selectedApp &&
-          <div className='absolute bg-slate-500/50 z-10 inset-0 flex justify-center items-center'>
-            <div className='px-10 py-6 bg-slate-900/95 rounded-lg'>
-              <div className='flex justify-between items-center'>
-                <div className='flex items-center'>
-                  <div className='w-16 text-sm'>name: </div>
-                  <div className='font-extrabold uppercase'>{selectedApp.name}</div>
-                </div>
-                <button>
-                  <Edit className='ml-3 h-4 w-4 stroke-slate-200' midFill="slate-200" />
-                </button>
-              </div>
-              <div className='flex justify-between items-center'>
-                <div className='flex items-center'>
-                  <div className='w-16 text-sm'>url: </div>
-                  <div className='font-extrabold'>{selectedApp.url}</div>
-                </div>
-                <button>
-                  <Edit className='ml-3 h-4 w-4 stroke-slate-200' midFill="slate-200" />
-                </button>
-              </div>
-              <div className='flex justify-between items-center'>
-                <div className='flex items-center'>
-                  <div className='w-16 text-sm'>logo url: </div>
-                  <div className='font-extrabold'>{selectedApp.logo}</div>
-                </div>
-                <button>
-                  <Edit className='ml-3 h-4 w-4 stroke-slate-200' midFill="slate-200" />
-                </button>
-              </div>
-              <div className='w-full mt-6 flex justify-end items-center'>
-                <button onClick={() => setSelectedApp(null)} className='mr-3 px-2 py-1 border border-slate-200 rounded-lg'>Close</button>
-                <button onClick={() => setSelectedApp(null)} className='px-2 py-1 text-slate-800 bg-slate-400 border border-slate-400 rounded-lg'>Apply</button>
-              </div>
+      </div>
+      {selectedApp &&
+        <div className='absolute p-6 z-10 inset-0 flex justify-center items-center bg-slate-500/50'>
+          <div className='w-full sm:w-[500px] px-10 py-6 bg-slate-900/95 rounded-lg'>
+            <div className='grid grid-cols-12 overflow-hidden text-ellipsis'>
+              <div className='col-span-12 sm:col-span-2 text-sm'>name: </div>
+              <div className='col-span-10 sm:col-span-9 font-extrabold uppercase text-ellipsis overflow-hidden'>{selectedApp.name}</div>
+              <button>
+                <Edit className='ml-3 h-4 w-4 stroke-slate-200' />
+              </button>
+              <div className='col-span-12 sm:col-span-2 text-sm'>url: </div>
+              <div className='col-span-10 sm:col-span-9 font-extrabold text-ellipsis overflow-hidden'>{selectedApp.url}</div>
+              <button>
+                <Edit className='ml-3 h-4 w-4 stroke-slate-200' />
+              </button>
+              <div className='col-span-12 sm:col-span-2 text-sm'>logo url: </div>
+              <div className='col-span-10 sm:col-span-9 w-full font-extrabold text-ellipsis overflow-hidden'>{selectedApp.logo}</div>
+              <button>
+                <Edit className='ml-3 h-4 w-4 stroke-slate-200' />
+              </button>
+            </div>
+            <div className='w-full mt-6 flex justify-end items-center'>
+              <button onClick={() => setSelectedApp(null)} className='mr-3 px-2 py-1 border border-slate-200 rounded-lg'>Close</button>
+              <button onClick={() => setSelectedApp(null)} className='px-2 py-1 text-slate-800 bg-slate-400 border border-slate-400 rounded-lg'>Apply</button>
             </div>
           </div>
-        }
-      </div>
+        </div>
+      }
+    </div>
   )
 }
 
--- src/icons/Icons.tsx
+++ src/icons/Icons.tsx
@@ -1,6 +1,5 @@
 interface IconProps {
     className?: string
-    midFill?: string
 }
 
 export const MoreInfo = (props: IconProps) => {
@@ -15,14 +14,16 @@ export const MoreInfo = (props: IconProps) => {
 }
 
 export const Edit = (props: IconProps) => {
-    const { className, midFill } = props
+    const { className } = props
     return (
-        <svg viewBox="0 0 24 24" className={className} fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg className={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">\
             <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
             <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-            <g id="SVGRepo_iconCarrier"> 
-                <path className={`fill-${midFill}`} fill-rule="evenodd" clip-rule="evenodd" d="M20.8477 1.87868C19.6761 0.707109 17.7766 0.707105 16.605 1.87868L2.44744 16.0363C2.02864 16.4551 1.74317 16.9885 1.62702 17.5692L1.03995 20.5046C0.760062 21.904 1.9939 23.1379 3.39334 22.858L6.32868 22.2709C6.90945 22.1548 7.44285 21.8693 7.86165 21.4505L22.0192 7.29289C23.1908 6.12132 23.1908 4.22183 22.0192 3.05025L20.8477 1.87868ZM18.0192 3.29289C18.4098 2.90237 19.0429 2.90237 19.4335 3.29289L20.605 4.46447C20.9956 4.85499 20.9956 5.48815 20.605 5.87868L17.9334 8.55027L15.3477 5.96448L18.0192 3.29289ZM13.9334 7.3787L3.86165 17.4505C3.72205 17.5901 3.6269 17.7679 3.58818 17.9615L3.00111 20.8968L5.93645 20.3097C6.13004 20.271 6.30784 20.1759 6.44744 20.0363L16.5192 9.96448L13.9334 7.3787Z"></path> 
+            <g id="SVGRepo_iconCarrier"> <g id="Edit / Edit_Pencil_01">
+                <path id="Vector" d="M12 8.00012L4 16.0001V20.0001L8 20.0001L16 12.0001M12 8.00012L14.8686 5.13146L14.8704 5.12976C15.2652 4.73488 15.463 4.53709 15.691 4.46301C15.8919 4.39775 16.1082 4.39775 16.3091 4.46301C16.5369 4.53704 16.7345 4.7346 17.1288 5.12892L18.8686 6.86872C19.2646 7.26474 19.4627 7.46284 19.5369 7.69117C19.6022 7.89201 19.6021 8.10835 19.5369 8.3092C19.4628 8.53736 19.265 8.73516 18.8695 9.13061L18.8686 9.13146L16 12.0001M12 8.00012L16 12.0001" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+            </g>
             </g>
         </svg>
     )
 }
+
